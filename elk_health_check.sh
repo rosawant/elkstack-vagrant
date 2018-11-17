988a49e04f979051c938812c493d83d88795ba2e
rosawant <roshansawant70@yahoo.com>
2018-11-17 20:23:46 +0530

    minor changes

--- elk_health_check.sh
+++ elk_health_check.sh
@@ -1,40 +1,32 @@
 #!/bin/sh
 
 #Configure the IP addresses
-IP=("IP1" "IP2" "IP3")
+IP="192.168.254.12 192.168.254.13"
 
 #Configure the hostnames
-hostname=("HOSTNAME1" "HOSTNAME2" "HOSTNAME3")
+HOSTNAMES="master agent"
 
-while true
+message=""
+flag=0
+for i in $IP
 do
-   message=""
-   flag=0
-   for (( i=0; i < ${#IP[@]}; i++ ))
-   do
-      echo $i
-      response=$(curl ${IP[$i]}:9200/_cluster/health)
-      if [[ ! "$response" =~ "green" ]]; then
-      flag=1
-         if [[ "$response" =~ "red" ]]; 
-         then
-            message+="Elasticsearch Server ${hostname[$i]}(${IP[$i]}) is down\n"
-         elif [[ "$response" =~ "yellow" ]]; then
-            message+="Elasticsearch  server ${hostname[$i]}(${IP[$i]}) shards are allocating\n"
-         elif [[ "$response" == "" ]]; then
-            message+="Elasticsearch process is not running in ${hostname[$i]}(${IP[$i]})\n"
-         fi
+   echo $i
+   response=$(curl ${IP[$i]}:9200/_cluster/health)
+   if [[ ! "$response" =~ "green" ]]; then
+   flag=1
+      if [[ "$response" =~ "red" ]]; 
+      then
+         message+="Elasticsearch Server ${hostname[$i]}(${IP[$i]}) is down\n"
+      elif [[ "$response" =~ "yellow" ]]; then
+         message+="Elasticsearch  server ${hostname[$i]}(${IP[$i]}) shards are allocating\n"
+      elif [[ "$response" == "" ]]; then
+         message+="Elasticsearch process is not running in ${hostname[$i]}(${IP[$i]})\n"
       fi
-   done
-
-   if [ $flag == 1 ]; then
-      echo "Sending Mail"
-
-      #Configure the E-Mail addresses
-      echo $message | mail -s "Pulse - Elasticssearch Server Down" EMailID1 EMailID2
-      echo $message
    fi
-
-   #Time duration between the monitoring. 10m represents 10 minutes
-   sleep 10m
 done
+
+if [ $flag == 1 ]; then
+   echo "Sending Mail"
+   echo $message | mail -s "Elasticssearch Server Down" root@localhost.com   
+fi
+
