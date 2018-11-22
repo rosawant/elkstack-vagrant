a00822ec574ac3f34d1cb539732edd816f243637
Roshan Sawant <Roshan.Sawant@nice.com>
2018-11-22 10:44:10 +0530

    changed elasticsearch conf

--- agent_setup.sh
+++ agent_setup.sh
@@ -8,7 +8,7 @@ curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.
 sudo dpkg -i elasticsearch-6.5.1.deb
 sudo chmod 755 -R /etc/elasticsearch
 
-sudo cp /vagrant/elasticsearch/elasticsearch.yml /etc/elasticsearch/
+#sudo cp /vagrant/elasticsearch/elasticsearch.yml /etc/elasticsearch/
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo /bin/systemctl start elasticsearch.service
@@ -19,14 +19,20 @@ sudo dpkg -i logstash-6.5.1.deb
 
 sudo cp -R /vagrant/logstash/* /etc/logstash/conf.d/
 
-sudo systemctl enable logstash.service
-sudo systemctl start logstash.service
+#sudo systemctl enable logstash.service
+#sudo systemctl start logstash.service
+sudo /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/filebeattest.conf &
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.5.1-amd64.deb
+sudo dpkg -i filebeat-6.5.1-amd64.deb
+
+sudo /usr/bin/filebeat -e -c /etc/logstash/conf.d/filebeat.yml &
 
 #kibana setup
 curl -L -O https://artifacts.elastic.co/downloads/kibana/kibana-6.5.1-amd64.deb
 sudo dpkg -i kibana-6.5.1-amd64.deb
 
-sudo cp /vagrant/kibana/kibana.yml /etc/kibana/
+#sudo cp /vagrant/kibana/kibana.yml /etc/kibana/
 
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable kibana.service
